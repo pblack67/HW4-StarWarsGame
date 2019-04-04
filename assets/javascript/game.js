43042f98cb7a4d43dd88d2e91df0280864e454d7
Paul Black <pblack67@comcast.net>
2019-04-04 10:47:08 -0500

    Code refactor for reuse and readability

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,9 +9,7 @@ function reset() {
     resetCharacters($("#playerCharacter"));
     resetCharacters($("#enemies"));
     resetCharacters($("#defender"));
-    $("#attackerMessage").text("");
-    $("#defenderMessage").text("");
-    $("#reset").hide();
+    setMessages("", "", false);
 }
 
 function resetCharacters(jqueryObject) {
@@ -26,111 +24,113 @@ function resetCharacters(jqueryObject) {
     }
 }
 
+function processCharacterClick(event) {
+    console.log("CHARACTER CLICK");
+    console.log("Selected Character:", $(this).text());
+
+    if ("characters" === $(this).parent().attr("id")) {
+        //      move/copy selected jedi to player character
+        $("#playerCharacter").append($(this));
+
+        //      move/copy rest of collection to enemies
+        var children = $("#characters").children();
+        for (var i = 0; i < children.length; i++) {
+            $("#enemies").append(children[i]);
+        }
+    } else if ("enemies" === $(this).parent().attr("id")) {
+        //      move character to defender
+        $("#defender").append($(this));
+        setMessages("", "", false);
+    }
+}
+
+function setMessages(attackerMessage, defenderMessage, showResetButton) {
+    $("#attackerMessage").text(attackerMessage);
+    $("#defenderMessage").text(defenderMessage);
+    if (showResetButton) {
+        $("#reset").show();
+    } else {
+        $("#reset").hide();
+    }
+}
+
+function attack(attacker, defender, attackAttributeName, isRegularAttack) {
+    var currentAttack = parseInt(attacker.attr(attackAttributeName));
+    var defenderHitPoints = parseInt(defender.attr("currentHitPoints"));
+    defenderHitPoints -= currentAttack;
+    console.log("Defender has", defenderHitPoints, "remaining");
+    defender.attr("currentHitPoints", defenderHitPoints);
+    updateHitPoints(defender, defenderHitPoints);
+
+    if (isRegularAttack) {
+        var defenderName = $(defender.children()[0]).text();
+        $("#attackerMessage").text("You attacked " + defenderName + " for " + currentAttack + " damage");
+        // increment currentAttack by baseAttack
+        var baseAttack = parseInt(attacker.attr("baseAttack"));
+        currentAttack += baseAttack;
+        console.log("Current Attack is now", currentAttack);
+        attacker.attr("currentAttack", currentAttack);
+    } else {
+        // Must be a counterattack
+        var attackerName = $(attacker.children()[0]).text();
+        $("#defenderMessage").text(attackerName + " attacked you for " + currentAttack + " damage");
+    }
+    return defenderHitPoints;
+}
+
+function processAttackClick(event) {
+    console.log("ATTACK!");
+
+    //      mumble if no enemy to attack (or just hide the silly thing)
+    // for now, just make sure there's one attacker and one defender
+    if ($("#playerCharacter").children().length === 1 &&
+        $("#defender").children().length === 1) {
+        var attacker = $($("#playerCharacter").children()[0]);
+        var defender = $($("#defender").children()[0]);
+
+        // make sure player still has hitpoints!
+        if (parseInt(attacker.attr("currentHitPoints")) > 0) {
+            // deal currentAttack damage to defender
+            var defenderHitPoints = attack(attacker, defender, "currentAttack", true);
+
+            //      if defender's hp's less then or equal to 0, remove defender
+            //      if no more defenders, VICTORY!!! else
+            if (defenderHitPoints <= 0) {
+                $(shadow).append(defender);
+
+                // check for victory
+                if ($("#enemies").children().length === 0 &&
+                    $("#defender").children().length === 0) {
+                    console.log("VICTORY!!!");
+                    setMessages("You defeated all the enemies! Well done! GAME OVER...", "", true);
+                } else {
+                    console.log("One enemy down!");
+                    setMessages("You defeated " + $(defender.children()[0]).text() + ". Choose another enemy to attack.", "", false);
+                }
+            } else {
+                //          deal defender's counterattack damage to player
+                var attackerHitPoints = attack(defender, attacker, "counterAttack", false);
+
+                //          if player's hp's less than or equal to 0, DEFEAT!
+                if (attackerHitPoints <= 0) {
+                    console.log("DEFEAT");
+                    setMessages("You were defeated. Better luck next time. GAME OVER...", "", true);
+                }
+            }
+        }
+    }
+}
 
 $(document).ready(function () {
 
     // on click of a character in characters
-    $(".jedi").on("click", function (event) {
-        console.log("CHARACTER CLICK");
-        console.log("Selected Character:", $(this).text());
-
-        if ("characters" === $(this).parent().attr("id")) {
-            //      move/copy selected jedi to player character
-            $("#playerCharacter").append($(this));
-
-            //      move/copy rest of collection to enemies
-            var children = $("#characters").children();
-            for (var i = 0; i < children.length; i++) {
-                $("#enemies").append(children[i]);
-            }
-        } else if ("enemies" === $(this).parent().attr("id")) {
-            //      move character to defender
-            $("#defender").append($(this));
-            $("#attackerMessage").text("");
-            $("#defenderMessage").text("");
-        }
-
-    });
+    $(".jedi").on("click", processCharacterClick);
 
     // on click of reset button
-    $("#reset").on("click", function (event) {
-        console.log("RESET");
-        reset();
-    });
+    $("#reset").on("click", reset);
 
     // on click of attack button
-    $("#attack").on("click", function () {
-        console.log("ATTACK!");
-
-        //      mumble if no enemy to attack (or just hide the silly thing)
-        // for now, just make sure there's one attacker and one defender
-        if ($("#playerCharacter").children().length === 1 &&
-            $("#defender").children().length === 1) {
-            var attacker = $($("#playerCharacter").children()[0]);
-            var defender = $($("#defender").children()[0]);
-
-            // make sure player still has hitpoints!
-            if (parseInt(attacker.attr("currentHitPoints")) > 0) {
-                // deal currentAttack damage to defender
-                var currentAttack = parseInt(attacker.attr("currentAttack"));
-                var defenderHitPoints = parseInt(defender.attr("currentHitPoints"));
-                defenderHitPoints -= currentAttack;
-                console.log("Defender has", defenderHitPoints, "remaining");
-                defender.attr("currentHitPoints", defenderHitPoints);
-                updateHitPoints(defender, defenderHitPoints);
-
-                var defenderName = $(defender.children()[0]).text();
-
-                // update playerattack message to the result
-                $("#attackerMessage").text("You attacked " + defenderName + " for " + currentAttack + " damage");
-
-                // increment currentAttack by baseAttack
-                var baseAttack = parseInt(attacker.attr("baseAttack"));
-                currentAttack += baseAttack;
-                console.log("Current Attack is now", currentAttack);
-                attacker.attr("currentAttack", currentAttack);
-
-
-                //      if defender's hp's less then or equal to 0, remove defender
-                //      if no more defenders, VICTORY!!! else
-                if (defenderHitPoints <= 0) {
-                    console.log("One enemy down!");
-                    $("#attackerMessage").text("You defeated " + defenderName + ". Choose another enemy to attack.");
-                    $("#defenderMessage").text("");
-                    $(shadow).append(defender);
-
-                    // check for victory
-                    if ($("#enemies").children().length === 0 &&
-                        $("#defender").children().length === 0) {
-                        console.log("VICTORY!!!");
-                        $("#attackerMessage").text("You defeated all the enemies! Well done! GAME OVER...");
-                        $("#defenderMessage").text("");
-                        $("#reset").show();
-                    }
-                } else {
-                    //          deal defender's counterattack damage to player
-                    var counterAttack = parseInt(defender.attr("counterAttack"));
-                    var attackerHitPoints = parseInt(attacker.attr("currentHitPoints"));
-                    attackerHitPoints -= counterAttack;
-                    attacker.attr("currentHitPoints", attackerHitPoints);
-                    console.log("Attacker has", attackerHitPoints, "remaining");
-                    updateHitPoints(attacker, attackerHitPoints);
-
-                    //          update defender's counterattack message to reflect the above
-                    $("#defenderMessage").text(defenderName + " attacked you for " + counterAttack + " damage");
-
-                    //          if player's hp's less than or equal to 0, DEFEAT!
-                    if (attackerHitPoints <= 0) {
-                        console.log("DEFEAT");
-                        $("#attackerMessage").text("You were defeated. Better luck next time. GAME OVER...");
-                        $("#defenderMessage").text("");
-                        $("#reset").show();
-                    }
-                }
-            }
-        }
-        //      if VICTORY or DEFEAT display reset button
-    });
+    $("#attack").on("click", processAttackClick);
 
     reset();
 });
