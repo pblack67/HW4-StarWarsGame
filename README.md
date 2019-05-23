2b9b83cbb5819732c3c453bf35ecda0f3c688ad4
Paul Black <pblack67@comcast.net>
2019-05-23 13:36:44 -0500

    Revised readme, fixed bug where clicking on a character would result in multiple defenders

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# HW4-jQueryGame
+# Star Wars Game
+
+* URL: [https://pblack67.github.io/StarWarsGame/](https://pblack67.github.io/StarWarsGame/)
+
+* Technologies: HTML, CSS, JavaScript, Events, jQuery
+
+## Overview
+
+This game simulates basic combat between characteres in a role-playing game. It's a Star Wars theme with several famous characters from over the years. The user selects a character to play. Then they select an enemy to battle from the remaining characters. The Attack button then reduces the hit points of the enemy while the enemy retaliates. The attack and retaliate values are different for each character. Also, the attack value for the character builds with each attack. The user wins if they are able to defeat the enemies before their hit points go to 0 or below.
+
+## Architecture
+
+The main program flow is in the game.js file. Application flow is driven by mouse click events on the character graphics and the attack button. Event handlers are attached via jQuery. Clicking on a character either moves it to the player character area or the defender area based on where the character graphic is. The attack button processes the attack logic and notifies the user whether their attack was successful or they ran out of hit points and lost. If the defender is defeated then it's moved to a shadow div so the application doesn't lose track of it. 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var shadow = $("<div>").attr("id", "shadow");
+let shadow = $("<div>").attr("id", "shadow");
 
 function updateHitPoints(jedi, hitPoints) {
     jedi.find('[class~="hitPoints"]').text(hitPoints + " HP");
@@ -13,9 +13,9 @@ function reset() {
 }
 
 function resetCharacters(jqueryObject) {
-    var children = jqueryObject.children();
-    for (var i = 0; i < children.length; i++) {
-        var child = children[i];
+    let children = jqueryObject.children();
+    for (let i = 0; i < children.length; i++) {
+        let child = children[i];
         // reset each character's values back to their initial settings
         $(child).attr("currentHitPoints", $(child).attr("baseHitPoints"));
         $(child).attr("currentAttack", $(child).attr("baseAttack"));
@@ -33,14 +33,16 @@ function processCharacterClick(event) {
         $("#playerCharacter").append($(this));
 
         //      move/copy rest of collection to enemies
-        var children = $("#characters").children();
-        for (var i = 0; i < children.length; i++) {
+        let children = $("#characters").children();
+        for (let i = 0; i <children.length; i++) {
             $("#enemies").append(children[i]);
         }
     } else if ("enemies" === $(this).parent().attr("id")) {
         //      move character to defender
-        $("#defender").append($(this));
-        setMessages("", "", false);
+        if ($("#defender").children().length == 0) {
+            $("#defender").append($(this));
+            setMessages("", "", false);
+        }
     }
 }
 
@@ -55,24 +57,24 @@ function setMessages(attackerMessage, defenderMessage, showResetButton) {
 }
 
 function attack(attacker, defender, attackAttributeName, isRegularAttack) {
-    var currentAttack = parseInt(attacker.attr(attackAttributeName));
-    var defenderHitPoints = parseInt(defender.attr("currentHitPoints"));
+    let currentAttack = parseInt(attacker.attr(attackAttributeName));
+    let defenderHitPoints = parseInt(defender.attr("currentHitPoints"));
     defenderHitPoints -= currentAttack;
     console.log("Defender has", defenderHitPoints, "remaining");
     defender.attr("currentHitPoints", defenderHitPoints);
     updateHitPoints(defender, defenderHitPoints);
 
     if (isRegularAttack) {
-        var defenderName = defender.find('[class~="jediname"]').text();
+        let defenderName = defender.find('[class~="jediname"]').text();
         $("#attackerMessage").text("You attacked " + defenderName + " for " + currentAttack + " damage");
         // increment currentAttack by baseAttack
-        var baseAttack = parseInt(attacker.attr("baseAttack"));
+        let baseAttack = parseInt(attacker.attr("baseAttack"));
         currentAttack += baseAttack;
         console.log("Current Attack is now", currentAttack);
         attacker.attr("currentAttack", currentAttack);
     } else {
         // Must be a counterattack
-        var attackerName = attacker.find('[class~="jediname"]').text();
+        let attackerName = attacker.find('[class~="jediname"]').text();
         $("#defenderMessage").text(attackerName + " attacked you for " + currentAttack + " damage");
     }
     return defenderHitPoints;
@@ -85,13 +87,13 @@ function processAttackClick(event) {
     // for now, just make sure there's one attacker and one defender
     if ($("#playerCharacter").children().length === 1 &&
         $("#defender").children().length === 1) {
-        var attacker = $($("#playerCharacter").children()[0]);
-        var defender = $($("#defender").children()[0]);
+        let attacker = $($("#playerCharacter").children()[0]);
+        let defender = $($("#defender").children()[0]);
 
         // make sure player still has hitpoints!
         if (parseInt(attacker.attr("currentHitPoints")) > 0) {
             // deal currentAttack damage to defender
-            var defenderHitPoints = attack(attacker, defender, "currentAttack", true);
+            let defenderHitPoints = attack(attacker, defender, "currentAttack", true);
 
             //      if defender's hp's less then or equal to 0, remove defender
             //      if no more defenders, VICTORY!!! else
@@ -109,7 +111,7 @@ function processAttackClick(event) {
                 }
             } else {
                 //          deal defender's counterattack damage to player
-                var attackerHitPoints = attack(defender, attacker, "counterAttack", false);
+                let attackerHitPoints = attack(defender, attacker, "counterAttack", false);
 
                 //          if player's hp's less than or equal to 0, DEFEAT!
                 if (attackerHitPoints <= 0) {
